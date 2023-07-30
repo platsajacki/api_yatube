8a4b8eb903eb97f23e9d053ea05d541cbfd3be5f
Вячеслав Менюхов <menyukhov@bk.ru>
2023-07-30 19:48:27 +0300

    posts/id/ is done.

--- yatube_api/api/serialisers.py
+++ yatube_api/api/serialisers.py
@@ -4,10 +4,7 @@ from posts.models import Post, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(
-        default=serializers.CurrentUserDefault(),
-        read_only=True
-    )
+    author = serializers.StringRelatedField(read_only=True)
     group = serializers.PrimaryKeyRelatedField(
         queryset=Group.objects.all(),
         required=False,
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 
 from .serialisers import PostSerializer
 from posts.models import Post
@@ -10,3 +11,13 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super().perform_destroy(instance)
